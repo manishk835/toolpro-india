2d59b4be88084fa71b77786daa63be63c27f617b
Manish Kumar <manishkumar.dev08@gmail.com>
2025-11-19 13:52:53 +0530

    add AdSense Script

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import '../globals.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Script from "next/script"
 
 export const metadata = {
   title: 'FastTools - Simple Indian Tools & AI Directory',
@@ -13,10 +14,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-7279809363476102"
+        />
+      </head>
+
       <body className="min-h-screen bg-slate-50 text-slate-900">
         <Header />
         <main className="max-w-4xl mx-auto p-4">{children}</main>
         <Footer />
+
+        {/* AdSense Script */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7279809363476102"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   )
